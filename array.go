1c49b533f2a742c5702bac18a2df3091a1e718e7
苏畅 <sotyo@icloud.com>
2020-12-17 14:38:39 +0900

    add Norm, Reduce, Std, Minus and unit test

--- array.go
+++ array.go
@@ -6,20 +6,51 @@ import (
 
 type Array []float64
 
-func (a Array) Add(b Array) Array {
+type D2Array [][]float64
+
+type FuncMap func(float64) float64
+
+type FuncReduce func(interface{}, float64, int) interface{}
+
+func (a Array) Add(b Array, rate float64) Array {
 	res := make(Array, len(a))
 	for i, v := range a {
-		res[i] = v + b[i]
+		res[i] = (v + b[i]) * rate
 	}
 	return res
 }
 
-func (a Array) Array() Array {
-	return []float64(a)
+func (a Array) Minus(b Array, rate float64) Array {
+	res := make(Array, len(a))
+	for i, v := range a {
+		res[i] = (v - b[i]) * rate
+	}
+	return res
+}
+
+func (a Array) Norm(power float64) float64 {
+	res := 0.0
+	for _, v := range a {
+		res += math.Pow(v, power)
+	}
+	return math.Pow(res, 1.0/power)
+}
+
+func (a Array) Std() float64 {
+	mean := a.Mean()
+	return math.Sqrt(a.Reduce(Array{}, func(acc interface{}, val float64, index int) interface{} {
+		res := acc.(Array)
+		res = append(res, (val-mean)*(val-mean))
+		return res
+	}).(Array).Reduce(0.0, func(acc interface{}, val float64, index int) interface{} {
+		res := acc.(float64)
+		res += val
+		return res
+	}).(float64) / float64(len(a)))
 }
 
 func (a Array) Diff() Array {
-	len := len(a.Array())
+	len := len(a)
 	res := make([]float64, len)
 	res[0] = math.NaN()
 	for i := 1; i < len; i++ {
@@ -35,7 +66,18 @@ func (a Array) Mean() (res float64) {
 	return res / float64(len(a))
 }
 
-func (a Array) Map(f func(v float64) float64) Array {
+func (a Array) Regularize(length int) Array {
+	std := a[:length].Std()
+	mean := a[:length].Mean()
+	res := a[:length].Reduce(Array{}, func(acc interface{}, val float64, index int) interface{} {
+		res := acc.(Array)
+		res = append(res, (val - mean)/std)
+		return res
+	}).(Array)
+	return append(res, a[length:]...)
+}
+
+func (a Array) Map(f FuncMap) Array {
 	res := make(Array, len(a))
 	for i, v := range a {
 		res[i] = f(v)
@@ -43,7 +85,7 @@ func (a Array) Map(f func(v float64) float64) Array {
 	return res
 }
 
-func (a Array) Reduce(initV interface{}, f func(acc interface{}, val float64, index int) interface{}) interface{} {
+func (a Array) Reduce(initV interface{}, f FuncReduce) interface{} {
 	res := initV
 	for i, v := range a {
 		res = f(res, v, i)
--- array_test.go
+++ array_test.go
@@ -1,15 +1,38 @@
 package array
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func Test_Add(t *testing.T) {
 	a := Array{1, 2, 3, 4, 5}
 	b := Array{5, 4, 3, 2, 1}
-	r := a.Add(b)
+	r := a.Add(b, 1)
 	for i, v := range r {
 		if v != 6 {
 			t.Errorf("Add [1,2,3,4,5] and [5,4,3,2,1] failed. Got %d at %f, expected 6.", i, v)
 		}
+	}
+}
+
+func Test_Mimus(t *testing.T) {
+	a := Array{1, 2, 3, 4, 5}
+	b := Array{1, 2, 3, 4, 5}
+	r := a.Minus(b, 1)
+	for i, v := range r {
+		if v != 0 {
+			t.Errorf("Add [1,2,3,4,5] and [5,4,3,2,1] failed. Got %d at %f, expected 6.", i, v)
+		}
+	}
+}
+
+func Test_NormDiff(t *testing.T) {
+	a := Array{6, 7}
+	b := Array{3, 3}
+	res := a.Minus(b, 1).Norm(2)
+	if res != 5 {
+		t.Errorf("norm of diff [6, 7, 5, 4] and [3, 3, 2, 0] failed. Got %f, expected 5.", res)
 	}
 }
 
@@ -34,5 +57,36 @@ func Test_Reduce(t *testing.T) {
 	}
 	if r2 != 55 {
 		t.Errorf("Reduce [1,2,3,4,5] failed. Got %f, expected %d.", r2, 55)
+	}
+}
+
+func Test_Std(t *testing.T) {
+	a := Array{1, 2, 3, 4, 5}
+	r := a.Std()
+	if math.Abs(r-1.414) > 0.01 {
+		t.Errorf("Reduce [1,2,3,4,5] failed. Got %f, expected 1.414214", r)
+	}
+}
+
+func Test_Regularize(t *testing.T) {
+	a := Array{1,2,3,4,5}
+	r := a.Regularize(4)
+	var vS float64
+	for i, v := range r {
+		switch i {
+		case 0:
+			vS = -1.3416
+		case 1:
+			vS = -0.4472
+		case 2:
+			vS = 0.4472
+		case 3:
+			vS = 1.3416
+		case 4:
+			vS = 5
+		}
+		if math.Abs(v - vS) > 0.01 {
+			t.Errorf("Regularize [1,2,3,4,5] failed. Got %f at %d, expected %f", v, i, vS)
+		}
 	}
 }
